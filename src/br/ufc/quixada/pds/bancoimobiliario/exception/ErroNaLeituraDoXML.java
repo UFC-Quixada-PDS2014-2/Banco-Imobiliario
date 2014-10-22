281e1a09ac448ec767c020461e9584e890109fb1
spectrus <spectrus@spectrus-POSITIVO-MOBILE>
2014-10-22 15:04:37 -0300

    Exceção de ErroNaLeituraDoXML adicionada na classe ConfiguracaoCasasTabuleiroComXStream

---
+++ src/br/ufc/quixada/pds/bancoimobiliario/exception/ErroNaLeituraDoXML.java
@@ -0,0 +1,7 @@
+package br.ufc.quixada.pds.bancoimobiliario.exception;
+
+public class ErroNaLeituraDoXML extends Exception{
+	public ErroNaLeituraDoXML(){
+		super("Erro na leitura do XML");
+	}
+}
