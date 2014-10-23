b81b44b6891f027d4a3abfacabbe17110cccb551
spectrus <spectrus@spectrus-POSITIVO-MOBILE>
2014-10-23 16:00:00 -0300

    Criação das interfaces Jogador e Tabuleiro

---
+++ src/br/ufc/quixada/pds/bancoimobiliario/model/Jogador.java
@@ -0,0 +1,14 @@
+package br.ufc.quixada.pds.bancoimobiliario.model;
+
+import java.util.List;
+
+public interface Jogador {
+
+
+	public void atualizarPosicao(int deslocamentoPosicao);
+	public String getNome();
+	public double getSaldo();
+	public int getPosicao();
+	public List<Logradouro> getLogradourosAdquiridos();
+	
+}
---
+++ src/br/ufc/quixada/pds/bancoimobiliario/model/Tabuleiro.java
@@ -0,0 +1,13 @@
+package br.ufc.quixada.pds.bancoimobiliario.model;
+
+import java.util.List;
+
+public interface Tabuleiro {
+
+
+	public List<Logradouro> getCasasDoTabuleiro();
+	public Logradouro pegarPontoDePartida();
+	public Logradouro percorrerTabuleiro(int posicaoBase, int deslocamento);
+	
+	
+}
