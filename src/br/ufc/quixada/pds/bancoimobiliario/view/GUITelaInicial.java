46a076d2c61737bd0ca1ca543d5af0a86102ece0
spectrus <spectrus@spectrus-POSITIVO-MOBILE>
2014-11-06 16:34:28 -0300

    Mudanças na GUITelaInicial.

--- src/br/ufc/quixada/pds/bancoimobiliario/view/GUITelaInicial.java
+++ src/br/ufc/quixada/pds/bancoimobiliario/view/GUITelaInicial.java
@@ -7,10 +7,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import java.awt.Color;
+import javax.swing.ImageIcon;
 
 public class GUITelaInicial extends JFrame {
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField nomeJogador1TxtArea;
+	private JTextField nomeJogador2TxtArea;
 	
 	public GUITelaInicial(){
 		
@@ -21,68 +22,84 @@ public class GUITelaInicial extends JFrame {
 	    getContentPane().setLayout(null);
 	    
 	    JLabel background = new JLabel();
-	    background.setIcon(null);
-	    background.setBounds(0, -28, 1150, 700);
-	    //getContentPane().add(background);
+	    background.setIcon(new ImageIcon(GUITelaInicial.class.getResource("/br/ufc/quixada/pds/bancoimobiliario/view/img/TelaInicialBackground.png")));
+	    background.setBounds(0, 0, 1150, 700);
+	    getContentPane().add(background);
 	    
 	    JButton tecnico1 = new JButton("");
 	    tecnico1.setIcon(null);
 	    tecnico1.setBackground(Color.WHITE);
-	    tecnico1.setBounds(64, 28, 117, 150);
-	    getContentPane().add(tecnico1);
+	    tecnico1.setBounds(64, 150, 117, 150);
+	    background.add(tecnico1);
 	    
 	    JButton tecnico2 = new JButton("");
 	    tecnico2.setBackground(Color.WHITE);
-	    tecnico2.setBounds(245, 28, 117, 150);
-	    getContentPane().add(tecnico2);
+	    tecnico2.setBounds(245, 150, 117, 150);
+	    background.add(tecnico2);
 	    
 	    JButton tecnico3 = new JButton("");
 	    tecnico3.setBackground(Color.WHITE);
-	    tecnico3.setBounds(426, 28, 117, 150);
-	    getContentPane().add(tecnico3);
+	    tecnico3.setBounds(426, 150, 117, 150);
+	    background.add(tecnico3);
 	    
 	    JButton tecnico4 = new JButton("");
 	    tecnico4.setBackground(Color.WHITE);
-	    tecnico4.setBounds(607, 28, 117, 150);
-	    getContentPane().add(tecnico4);
+	    tecnico4.setBounds(607, 150, 117, 150);
+	    background.add(tecnico4);
 	    
 	    JButton tecnico5 = new JButton("");
 	    tecnico5.setBackground(Color.WHITE);
-	    tecnico5.setBounds(788, 28, 117, 150);
-	    getContentPane().add(tecnico5);
+	    tecnico5.setBounds(788, 150, 117, 150);
+	    background.add(tecnico5);
 	    
 	    JButton tecnico6 = new JButton("");
 	    tecnico6.setBackground(Color.WHITE);
-	    tecnico6.setBounds(969, 28, 117, 150);
-	    getContentPane().add(tecnico6);
+	    tecnico6.setBounds(969, 150, 117, 150);
+	    background.add(tecnico6);
 	    
-	    textField = new JTextField();
-	    textField.setBounds(40, 440, 169, 33);
-	    getContentPane().add(textField);
-	    textField.setColumns(10);
+	    nomeJogador1TxtArea = new JTextField();
+	    nomeJogador1TxtArea.setBounds(40, 440, 169, 33);
+	    background.add(nomeJogador1TxtArea);
+	    nomeJogador1TxtArea.setColumns(10);
 	    
-	    JButton button = new JButton("");
-	    button.setBackground(Color.WHITE);
-	    button.setBounds(40, 493, 117, 150);
-	    getContentPane().add(button);
+	    JButton jogador1Selecionado = new JButton("");
+	    jogador1Selecionado.setBackground(Color.WHITE);
+	    jogador1Selecionado.setBounds(40, 493, 117, 150);
+	    background.add(jogador1Selecionado);
 	    
-	    JLabel lblNewLabel = new JLabel("Nome jogador 1:");
-	    lblNewLabel.setBounds(40, 413, 119, 20);
-	    getContentPane().add(lblNewLabel);
+	    JLabel nomeJogador1Label = new JLabel("Nome jogador 1:");
+	    nomeJogador1Label.setBounds(40, 413, 119, 20);
+	    background.add(nomeJogador1Label);
 	    
-	    JButton button_1 = new JButton("");
-	    button_1.setBackground(Color.WHITE);
-	    button_1.setBounds(993, 493, 117, 150);
-	    getContentPane().add(button_1);
+	    JButton jogador2Selecionado = new JButton("");
+	    jogador2Selecionado.setBackground(Color.WHITE);
+	    jogador2Selecionado.setBounds(993, 493, 117, 150);
+	    background.add(jogador2Selecionado);
 	    
-	    textField_1 = new JTextField();
-	    textField_1.setColumns(10);
-	    textField_1.setBounds(941, 440, 169, 33);
-	    getContentPane().add(textField_1);
+	    nomeJogador2TxtArea = new JTextField();
+	    nomeJogador2TxtArea.setColumns(10);
+	    nomeJogador2TxtArea.setBounds(941, 440, 169, 33);
+	    background.add(nomeJogador2TxtArea);
 	    
-	    JLabel label = new JLabel("Nome jogador 2:");
-	    label.setBounds(991, 413, 119, 20);
-	    getContentPane().add(label);
+	    JLabel nomeJogador2Label = new JLabel("Nome jogador 2:");
+	    nomeJogador2Label.setBounds(991, 413, 119, 20);
+	    background.add(nomeJogador2Label);
+	    
+	    JButton alterarJogador1 = new JButton("Alterar");
+	    alterarJogador1.setBounds(40, 650, 117, 25);
+	    background.add(alterarJogador1);
+	    
+	    JButton alterarJogador2 = new JButton("Alterar");
+	    alterarJogador2.setBounds(993, 650, 117, 25);
+	    background.add(alterarJogador2);
+	    
+	    JButton iniciarJogo = new JButton();
+	    iniciarJogo.setEnabled(false);
+	    iniciarJogo.setBounds(553, 450, 50, 50);
+	    iniciarJogo.setBorder(null);
+	    iniciarJogo.setOpaque(false);
+	    background.add(iniciarJogo);
+	    
 	    this.setResizable(false);
 	    this.setVisible(true);
 	}
