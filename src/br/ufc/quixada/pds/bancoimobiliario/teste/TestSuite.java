f9ba16e8a08e5e1147c24e74ddf929e790b5ac0f
Felipe Neves Bito <felipenevesbrito@gmail.com>
2014-11-06 21:29:08 -0300

    Classe JogadorTeste sumiu !!!

--- src/br/ufc/quixada/pds/bancoimobiliario/teste/TestSuite.java
+++ src/br/ufc/quixada/pds/bancoimobiliario/teste/TestSuite.java
@@ -11,7 +11,7 @@ import org.junit.runners.Suite;
    AcaoVoltaPosicaoTeste.class,
    EmpresaTeste.class,
    ImovelTeste.class,
-   JogadorTeste.class,
+  // JogadorTeste.class,
    PontoDePartidaTeste.class,
    PropriedadeTeste.class  
 })
